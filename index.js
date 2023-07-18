a4ded0f1eaae1f0fbf86b348280bf83cbb23d96d
Yunaks <64258925+Yunaks@users.noreply.github.com>
2023-07-18 15:04:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,37 +34,46 @@ await guildCreate(token)
 })
 
 
-async function guildCreate(token) {  
+async function guildCreate(token) {
     try {
-        await axios.post(
+        const response = await axios.post(
             'https://discord.com/api/v9/guilds',
             {
-              'name': 'Yunak Server Cloner',
+              'name': 'Yunak Clone Server',
               'icon': null,
               'channels': guildchannels,
-              "roles" : guildroles,
+              'roles':guildroles,
               'system_channel_id': null,
               'guild_template_code': '2TffvPucqHkN'
             },
             {
               headers: {
+                'authority': 'discord.com',
+                'accept': '*/*',
+                'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+                'authorization': token.replace(/\r?\n|\r/g, ''),
+                'content-type': 'application/json',
+                'origin': 'https://discord.com',
+                'referer': 'https://discord.com/guild-discovery',
                 'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-                'X-Debug-Options': 'bugReporterEnabled',
                 'sec-ch-ua-mobile': '?0',
-                'Authorization': token.replace(/\r?\n|\r/g, ''),
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-                'X-Discord-Timezone': 'Europe/Istanbul',
-                'Content-Type': 'application/json',
-                'Referer': 'https://discord.com/channels/@me',
-                'X-Discord-Locale': 'tr',
-                'sec-ch-ua-platform': '"Windows"'
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'x-debug-options': 'bugReporterEnabled',
+                'x-discord-locale': 'tr',
+                'x-discord-timezone': 'Europe/Istanbul',
+                'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6InRyLVRSIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExNC4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTE0LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiJodHRwczovL2VudHJvcHkuY2x1Yi8iLCJyZWZlcnJpbmdfZG9tYWluIjoiZW50cm9weS5jbHViIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjIxMzY5NSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0='
               }
             }
           );
-        console.log(`copy guild created!`)
-        process.exit();
+  
+      console.log('successfully server');
+      process.exit();
     } catch (error) {
-      console.log('error !');
+      console.log('error:', error);
       process.exit();
     }
   }
